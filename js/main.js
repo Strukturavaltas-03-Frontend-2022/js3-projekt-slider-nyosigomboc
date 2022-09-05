61787bda4544150e0b1a98850c7299ecf05346f1
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-05 23:38:49 +0200

    almost done

---
+++ js/main.js
@@ -0,0 +1,77 @@
+import {
+  getNext, getPrev, getNth, getNumberOfImages, getStatus,
+// eslint-disable-next-line import/extensions
+} from './pictures.js'; // eslint doesn't know this, but it won't work without .js extension
+
+const currentImageImg = document.querySelector('#img img');
+const oldImageImg = document.querySelector('#old_img img');
+const captionDiv = document.querySelector('.caption');
+const statusDiv = document.querySelector('.status');
+
+const loadTime = 100;
+const changeTime = 500;
+
+const updateStatus = () => {
+  statusDiv.innerHTML = getStatus();
+};
+
+const changeImage = (url, caption) => {
+  oldImageImg.setAttribute('src', currentImageImg.getAttribute('src'));
+  currentImageImg.style.opacity = 0;
+  currentImageImg.setAttribute('src', url);
+  currentImageImg.setAttribute('alt', caption);
+  captionDiv.innerHTML = caption;
+  updateStatus();
+  setTimeout(() => {
+    currentImageImg.style.opacity = 1;
+    setTimeout(() => {
+      oldImageImg.setAttribute('src', url);
+    }, changeTime);
+  }, loadTime); // leave some time to load the image
+};
+
+const changeImageObj = (obj) => {
+  const { src, caption } = obj;
+  changeImage(src, caption);
+};
+
+const loadNext = () => {
+  changeImageObj(getNext());
+};
+
+const loadPrev = () => {
+  changeImageObj(getPrev());
+};
+
+const loadNth = (n) => {
+  changeImageObj(getNth(n));
+};
+
+loadNth(0);
+document.querySelector('.go_left').addEventListener('click', loadPrev);
+document.querySelector('.go_right').addEventListener('click', loadNext);
+
+const createCircles = (n) => {
+  let circles = '';
+  for (let i = 0; i < n; i += 1) {
+    circles += '<span class="dot"></span>\n';
+  }
+  document.querySelector('.chooser').innerHTML = circles;
+};
+
+const addListeners = () => {
+  document.querySelectorAll('.dot').forEach((element, index) => {
+    element.addEventListener('click', () => {
+      loadNth(index);
+    });
+  });
+};
+
+const changeHeight = (height) => {
+  document.querySelector('.gallery').style.height = height;
+};
+
+createCircles(getNumberOfImages());
+addListeners();
+
+// changeHeight('100px');
--- js/pictures.js
+++ js/pictures.js
@@ -1,15 +1,55 @@
 const pictures = [
   {
-    src: 'http://...',
-    caption: 'valami',
+    src: 'images/2022 JUNIOR U23 WCH Photo Finish 164.jpg',
+    caption: 'Lucz Anna gold medal photofinish',
   },
   {
-    src: 'http://...',
-    caption: 'valami',
+    src: 'images/IMG_20220830_180440.jpg',
+    caption: 'Opening ceremony',
   },
   {
-    src: 'http://...',
-    caption: 'valami',
+    src: 'images/IMG_20220831_140519.jpg',
+    caption: 'Rescue boats at the finish line',
+  },
+  {
+    src: 'images/IMG_20220903_101019.jpg',
+    caption: 'Another gold medal',
+  },
+  {
+    src: 'images/IMG_20220903_145756.jpg',
+    caption: 'Yet another gold medal',
+  },
+  {
+    src: 'images/IMG_20220904_094451.jpg',
+    caption: 'My stuff',
+  },
+  {
+    src: 'images/IMG_20220904_171908.jpg',
+    caption: '5000m turn',
   },
 ];
 
+let currentIndex = 0;
+const numberOfImages = pictures.length;
+
+export const getCurrentIndex = () => currentIndex;
+export const getNumberOfImages = () => numberOfImages;
+
+export const getCurrent = () => pictures[currentIndex];
+
+export const getNext = () => {
+  currentIndex = (currentIndex + numberOfImages + 1) % numberOfImages;
+  return getCurrent();
+};
+
+export const getPrev = () => {
+  currentIndex = (currentIndex + numberOfImages - 1) % numberOfImages;
+  return getCurrent();
+};
+
+export const getNth = (n) => {
+  currentIndex = n;
+  return getCurrent();
+};
+
+export const getStatus = () => `${getCurrentIndex() + 1}/${getNumberOfImages()}`;
